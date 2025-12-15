243eb8c8eaafee09641b7d34a275d7d9b9020f4e
Артём <rem@mail.ru>
2025-12-15 10:34:24 +0300

    Начальный коммит - вся настройка

---
+++ app/build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.devtools.ksp)
+}
+
+android {
+    namespace = "com.example.up_piatnitskii"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.up_piatnitskii"
+        minSdk = 33
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    // Для работы с камерой и галереей
+    implementation("com.google.accompanist:accompanist-permissions:0.30.1")
+    implementation("io.coil-kt.coil3:coil-compose:3.2.0")
+
+    // Для работы с геоданными
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+
+    // SplashScreen
+    implementation("androidx.core:core-splashscreen:1.2.0")
+
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.retrofit)
+    implementation(libs.gson)
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+}
---
+++ app/src/main/java/com/example/up_piatnitskii/data/RetrofitInstance.kt
@@ -0,0 +1,22 @@
+package com.example.up_piatnitskii.data
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.net.InetSocketAddress
+import java.net.Proxy
+
+object RetrofitInstance {
+    const val SUPABASE_URL = "https://yyqirkphokkzroxdraim.supabase.co/"
+
+    var proxy: Proxy = Proxy(Proxy.Type.HTTP, InetSocketAddress("10.207.106.77", 3128))
+    var client: OkHttpClient = OkHttpClient.Builder().proxy(proxy).build()
+
+    //var client: OkHttpClient = OkHttpClient.Builder().build()
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(SUPABASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+    //val userManagementService = retrofit.create((UserManagementService::class.java))
+}
